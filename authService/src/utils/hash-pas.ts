136bdd430b07f64e9e167fa9ef52aabd10fc31f4
jaquelineatienza <jaquibatienza.com>
2025-04-23 12:07:14 -0300

    se eliminaronlas carpetas no correspondientes y se creo la logica para subir user sin avatar(por momento opcional)

--- authService/src/utils/hash-pas.ts
+++
@@ -1,9 +0,0 @@
-import { genSalt, hash } from "bcrypt";
-
-
-export async function hashPassword(password: string): Promise<string> {
-    const salt = await genSalt(10);
-    const hashed = await hash(password, salt);
-    return hashed;
-}
-
