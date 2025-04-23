487dcfcb3ea5290d6689cbd458495ca70b6e6208
jaquelineatienza <jaquibatienza.com>
2025-04-23 15:23:15 -0300

    se elimino las carpetas extreas

--- user_service/src/routes/userService.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { validateUser } from "../validations/user.validations";
-
-import { createUsers } from "../controllers/user.controllers";
-import { checkValidationErrors } from "../middlewares/express-validator";
-
-export const userRoutes = Router();
-
-userRoutes.post("/user", createUsers);
