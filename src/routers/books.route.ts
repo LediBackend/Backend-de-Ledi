78457a11ea89ecd471c1a403192c4ee9ebd45ae9
ElBrunoX.js <brunolok79@gmail.com>
2025-05-08 00:34:56 -0300

    se añadio el middleware en la ruta que resive los archivos

--- src/routers/books.route.ts
+++ src/routers/books.route.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { createBooks, deleteBooks, getBooks, getBooksById } from "../controllers/Books.controller";
-
+import upload from "../middleware/stotage";
 const routerBooks = Router();
 
 routerBooks.get("/Books", getBooks);
-routerBooks.post("/Books", createBooks);
+routerBooks.post(
+  "/Books",
+  upload.fields([
+    { name: "file", maxCount: 1 },
+    { name: "img", maxCount: 1 },
+  ]),
+  createBooks
+);
 routerBooks.get("/Books/:id", getBooksById);
 routerBooks.delete("/Books/:id", deleteBooks);
 
