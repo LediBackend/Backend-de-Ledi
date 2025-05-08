1018bb2f3ab276053ac9a41e11474552d048c95a
ElBrunoX.js <brunolok79@gmail.com>
2025-05-08 00:43:25 -0300

    se implemento la funcionalidad de crear el directorio de uploads para almacenar todo los archivos que le vengan por cuerpo para su procesado

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import cors from "cors";
 import morgan from "morgan";
 import express from "express";
-import fileUpload from "express-fileupload";
+import fs from "fs";
+import path from "path";
 import ENV from "./config/env";
 import routerBooks from "./routers/books.route";
 import routerContentBooks from "./routers/contentBook.route";
@@ -10,16 +11,15 @@ import chalk from "chalk";
 
 const app = express();
 
-app.use(
-  fileUpload({
-    useTempFiles: true,
-    tempFileDir: "./src/tmp/",
-  })
-);
+const fileUpload = path.join(__dirname, "./uploads");
+if (!fs.existsSync(fileUpload)) {
+  fs.mkdirSync(fileUpload, { recursive: true });
+}
 
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static("./src/uploads"));
 app.use(routerBooks);
 app.use(routerContentBooks);
 
